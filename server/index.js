8b0fa353197557ecbfc2381608343df2d88ea5c6
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-07-30 22:59:09 +0300

    Modify server

--- server/index.js
+++ server/index.js
@@ -1,35 +1,33 @@
+import cors from "cors";
+
+import express, { json, urlencoded } from "express";
+
+
+import { uploader } from "./cloudinary/cloudinary";
+
 require("dotenv").config();
-
-const cors = require("cors");
-
-const express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT;
 
-
-const cloudinary = require("./cloudinary/cloudinary");
-
 app.use(cors());
 
-app.use(express.json({ limit: "50mb" }));
+app.use(json({ limit: "50mb" }));
 
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-
-app.use(express.json({ limit: "50mb" }));
-
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(urlencoded({ limit: "50mb", extended: true }));
 
 
-app.get('/api/v1/login', (req, res, next) => {
-    res.json({ok: true});
-})
+app.use(json({ limit: "50mb" }));
+
+app.use(urlencoded({ limit: "50mb", extended: true }));
+
+
+
 
 app.post("/api/v1/upload", async (req, res) => {
   let { urlData } = req.body;
-  const uploadedImage = await cloudinary.uploader.upload(
+  const uploadedImage = await uploader.upload(
     urlData,
     {
     //   upload_preset: "unsigned_upload",
@@ -49,15 +47,6 @@ app.post("/api/v1/upload", async (req, res) => {
     console.log(error);
   }
 });
-
-app.post("/", async (req, res) => {
-  res.json("I have received the data");
-});
-
-app.patch("/", (req, res) => {});
-
-app.delete("/", (req, res) => {});
-
 
 
 
