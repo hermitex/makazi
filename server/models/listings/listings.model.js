fac66866a084b957c718938b272ba53114c354c2
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-07-30 22:58:46 +0300

    add listing model

--- server/models/listings/listings.model.js
+++ server/models/listings/listings.model.js
@@ -1,15 +1,27 @@
-const porpertySchema = new Schema({
-  name: String,
-  address: String,
-  summary: String,
-  city: String,
-  county: String,
-  category: String,
-  type: String,
-  description: String,
-  img_url: String,
-  bedrooms: Number,
-  bathrooms: Number,
-  size: Number,
-  price: Number,
-});
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema(
+  {
+    name: { type: String, require: true },
+    summary: { type: String, require: true },
+    city: { type: String, require: true },
+    county: { type: String, require: true },
+    category: { type: String, require: true },
+    type: { type: String, require: true },
+    description: { type: String, require: true },
+    img_url: { type: String, require: true },
+    bedrooms: { type: Number, required: true },
+    bathrooms: { type: Number, required: true },
+    size: { type: Number, required: true },
+    price: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Listing = mongoose.model("listings", listingSchema);
+
+export default Listing;
