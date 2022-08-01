86a9a3f2acb0252a99e459d8051f34ad90001b38
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-08-01 15:32:00 +0300

    Add back button to listing details

--- src/components/properties/ListingDetails.jsx
+++ src/components/properties/ListingDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import {
   Card,
   CardBody,
@@ -9,6 +9,7 @@ import {
   ListGroup,
   ListGroupItem,
   Row,
+  Button,
 } from "reactstrap";
 import Profile from "../profile/Profile";
 
@@ -36,92 +37,102 @@ function ListingDetails({}) {
         {/* <Card className="h-100">
           <img src={imgUrl} alt="listing" />
         </Card> */}
-        <Profile/>
+        <Profile />
       </Col>
       <Col md={9}>
-      <Card className="p-5">
-        <Row>
-        <Col md={3}>
-        <Card className="h-100">
-          <img src={imgUrl} alt="listing" />
-        </Card>
-      
-      </Col>
-      <Col md={9}>
-    
-          <CardTitle className="h2 text-muted">{name}</CardTitle>
-          <CardBody>
-            <CardText>{description}</CardText>
-            <div className="container-fluid">
-              <Row className="my-5 ">
-                <Col md={4} className="">
-                  <Card className="bg-dark py-3 px-1">
-                    <CardBody>
-                      <CardText className="text-muted h5"> Offering at </CardText>
-                      <CardTitle className="h4 text-light">{price}</CardTitle>
-                    </CardBody>
-                  </Card>
-                </Col>
-                <Col md={4}>
-                  <Card className="bg-dark py-3 px-1">
-                    <CardBody>
-                      
-                      <CardText className="text-muted h5">
-                      Location
-                      </CardText>
-                      <CardTitle className="h4 text-light"> {city}</CardTitle>
-                    </CardBody>
-                  </Card>
-                </Col>
-                <Col md={4}>
-                  <Card className="bg-dark py-3 px-1">
-                    <CardBody>
-                    <CardText className="text-muted h5">Category</CardText>
-                      <CardTitle className="h4 text-light">{category}</CardTitle>
-                     
-                    </CardBody>
-                  </Card>
-                </Col>
-              </Row>
-            </div>
-            <ListGroup
-              horizontal
-              className="d-flex text-center"
-              style={{
-                width: "100%",
-                marginTop: "2rem",
-              }}
-            >
-              <ListGroupItem
-                style={{
-                  width: "100%",
-                }}
-              >
-                <i className="fa-solid fa-bed"></i>
-                {listing.bedrooms}
-              </ListGroupItem>
-              <ListGroupItem
-                style={{
-                  width: "100%",
-                }}
-              >
-                <i className="fa-solid fa-shower"></i>
-                {listing.bathrooms}
-              </ListGroupItem>
-              <ListGroupItem
-                style={{
-                  width: "100%",
-                }}
-              >
-                <var>{listing.size}</var>
-                <sup>2</sup> ft
-              </ListGroupItem>
-            </ListGroup>
-          </CardBody>
-       
-        </Col>
-        
-        </Row>
+        <Card className="p-5">
+          <Row>
+            <Col md={3}>
+              <Link to="/listings">
+                <i class="fa-solid fa-chevron-left"></i>
+                <Button>Back</Button>
+              </Link>
+              <Card className="h-100">
+                <img src={imgUrl} alt="listing" />
+              </Card>
+            </Col>
+            <Col md={9}>
+              <CardTitle className="h2 text-muted">{name}</CardTitle>
+              <CardBody>
+                <CardText>{description}</CardText>
+                <div className="container-fluid">
+                  <Row className="my-5 ">
+                    <Col md={4} className="">
+                      <Card className="bg-dark py-3 px-1">
+                        <CardBody>
+                          <CardText className="text-muted h5">
+                            {" "}
+                            Offering at{" "}
+                          </CardText>
+                          <CardTitle className="h4 text-light">
+                            {price}
+                          </CardTitle>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    <Col md={4}>
+                      <Card className="bg-dark py-3 px-1">
+                        <CardBody>
+                          <CardText className="text-muted h5">
+                            Location
+                          </CardText>
+                          <CardTitle className="h4 text-light">
+                            {" "}
+                            {city}
+                          </CardTitle>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    <Col md={4}>
+                      <Card className="bg-dark py-3 px-1">
+                        <CardBody>
+                          <CardText className="text-muted h5">
+                            Category
+                          </CardText>
+                          <CardTitle className="h4 text-light">
+                            {category}
+                          </CardTitle>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                </div>
+                <ListGroup
+                  horizontal
+                  className="d-flex text-center"
+                  style={{
+                    width: "100%",
+                    marginTop: "2rem",
+                  }}
+                >
+                  <ListGroupItem
+                    style={{
+                      width: "100%",
+                    }}
+                  >
+                    <i className="fa-solid fa-bed"></i>
+                    {listing.bedrooms}
+                  </ListGroupItem>
+                  <ListGroupItem
+                    style={{
+                      width: "100%",
+                    }}
+                  >
+                    <i className="fa-solid fa-shower"></i>
+                    {listing.bathrooms}
+                  </ListGroupItem>
+                  <ListGroupItem
+                    style={{
+                      width: "100%",
+                    }}
+                  >
+                    <var>{listing.size}</var>
+                    <sup>2</sup> ft
+                  </ListGroupItem>
+                </ListGroup>
+              </CardBody>
+            </Col>
+          </Row>
         </Card>
       </Col>
     </Row>
