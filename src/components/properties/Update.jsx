85c5b22f9f93e70e35994b05abbd6ea0f3a3b246
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-08-01 16:02:01 +0300

    fix updating

--- src/components/properties/Update.jsx
+++ src/components/properties/Update.jsx
@@ -103,6 +103,7 @@ function NewListingForm({
   }
 
   async function postListing(data) {
+    console.log(data);
     try {
       await axios.patch(
         `https://makazi-api.herokuapp.com/api/v1/listings/${id}`,
