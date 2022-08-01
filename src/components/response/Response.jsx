530fdbd30d9c2abbd3e8f19800a0fa971e8893bb
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-08-01 17:37:25 +0300

    fix redirect

--- src/components/response/Response.jsx
+++ src/components/response/Response.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Response({ message, type, redirect }) {
   return (
     <div className={"text-light py-2 px-1 bg-" + type}>
-      <small> {message}! </small>
+      <small> {message} </small>
     </div>
   );
 }
