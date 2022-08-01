a73f5b4d884e3b588843287c83063275dce8538d
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-08-01 15:38:37 +0300

    modify back button layout

--- src/components/properties/ListingDetails.jsx
+++ src/components/properties/ListingDetails.jsx
@@ -34,104 +34,111 @@ function ListingDetails({}) {
   return (
     <Row className="my-5 container-fluid">
       <Col md={3}>
-        {/* <Card className="h-100">
-          <img src={imgUrl} alt="listing" />
-        </Card> */}
         <Profile />
       </Col>
       <Col md={9}>
         <Card className="p-5">
           <Row>
-            <Col md={3}>
+            <Col md={12}>
               <Link to="/listings">
-                <i class="fa-solid fa-chevron-left"></i>
-                <Button>Back</Button>
+                <Button>
+                  <i class="fa-solid fa-chevron-left"></i> Back
+                </Button>
               </Link>
-              <Card className="h-100">
-                <img src={imgUrl} alt="listing" />
-              </Card>
             </Col>
-            <Col md={9}>
-              <CardTitle className="h2 text-muted">{name}</CardTitle>
-              <CardBody>
-                <CardText>{description}</CardText>
-                <div className="container-fluid">
-                  <Row className="my-5 ">
-                    <Col md={4} className="">
-                      <Card className="bg-dark py-3 px-1">
-                        <CardBody>
-                          <CardText className="text-muted h5">
-                            {" "}
-                            Offering at{" "}
-                          </CardText>
-                          <CardTitle className="h4 text-light">
-                            {price}
-                          </CardTitle>
-                        </CardBody>
-                      </Card>
-                    </Col>
-                    <Col md={4}>
-                      <Card className="bg-dark py-3 px-1">
-                        <CardBody>
-                          <CardText className="text-muted h5">
-                            Location
-                          </CardText>
-                          <CardTitle className="h4 text-light">
-                            {" "}
-                            {city}
-                          </CardTitle>
-                        </CardBody>
-                      </Card>
-                    </Col>
-                    <Col md={4}>
-                      <Card className="bg-dark py-3 px-1">
-                        <CardBody>
-                          <CardText className="text-muted h5">
-                            Category
-                          </CardText>
-                          <CardTitle className="h4 text-light">
-                            {category}
-                          </CardTitle>
-                        </CardBody>
-                      </Card>
-                    </Col>
-                  </Row>
-                </div>
-                <ListGroup
-                  horizontal
-                  className="d-flex text-center"
-                  style={{
-                    width: "100%",
-                    marginTop: "2rem",
-                  }}
-                >
-                  <ListGroupItem
+            <Row>
+              <Col md={3}>
+                <Link to="/listings">
+                  <Button>
+                    <i class="fa-solid fa-chevron-left"></i> Back
+                  </Button>
+                </Link>
+                <Card className="h-100">
+                  <img src={imgUrl} alt="listing" />
+                </Card>
+              </Col>
+              <Col md={9}>
+                <CardTitle className="h2 text-muted">{name}</CardTitle>
+                <CardBody>
+                  <CardText>{description}</CardText>
+                  <div className="container-fluid">
+                    <Row className="my-5 ">
+                      <Col md={4} className="">
+                        <Card className="bg-dark py-3 px-1">
+                          <CardBody>
+                            <CardText className="text-muted h5">
+                              {" "}
+                              Offering at{" "}
+                            </CardText>
+                            <CardTitle className="h4 text-light">
+                              {price}
+                            </CardTitle>
+                          </CardBody>
+                        </Card>
+                      </Col>
+                      <Col md={4}>
+                        <Card className="bg-dark py-3 px-1">
+                          <CardBody>
+                            <CardText className="text-muted h5">
+                              Location
+                            </CardText>
+                            <CardTitle className="h4 text-light">
+                              {" "}
+                              {city}
+                            </CardTitle>
+                          </CardBody>
+                        </Card>
+                      </Col>
+                      <Col md={4}>
+                        <Card className="bg-dark py-3 px-1">
+                          <CardBody>
+                            <CardText className="text-muted h5">
+                              Category
+                            </CardText>
+                            <CardTitle className="h4 text-light">
+                              {category}
+                            </CardTitle>
+                          </CardBody>
+                        </Card>
+                      </Col>
+                    </Row>
+                  </div>
+                  <ListGroup
+                    horizontal
+                    className="d-flex text-center"
                     style={{
                       width: "100%",
+                      marginTop: "2rem",
                     }}
                   >
-                    <i className="fa-solid fa-bed"></i>
-                    {listing.bedrooms}
-                  </ListGroupItem>
-                  <ListGroupItem
-                    style={{
-                      width: "100%",
-                    }}
-                  >
-                    <i className="fa-solid fa-shower"></i>
-                    {listing.bathrooms}
-                  </ListGroupItem>
-                  <ListGroupItem
-                    style={{
-                      width: "100%",
-                    }}
-                  >
-                    <var>{listing.size}</var>
-                    <sup>2</sup> ft
-                  </ListGroupItem>
-                </ListGroup>
-              </CardBody>
-            </Col>
+                    <ListGroupItem
+                      style={{
+                        width: "100%",
+                      }}
+                    >
+                      <i className="fa-solid fa-bed"></i>
+                      {listing.bedrooms}
+                    </ListGroupItem>
+                    <ListGroupItem
+                      style={{
+                        width: "100%",
+                      }}
+                    >
+                      <i className="fa-solid fa-shower"></i>
+                      {listing.bathrooms}
+                    </ListGroupItem>
+                    <ListGroupItem
+                      style={{
+                        width: "100%",
+                      }}
+                    >
+                      <var>{listing.size}</var>
+                      <sup>2</sup> ft
+                    </ListGroupItem>
+                  </ListGroup>
+                </CardBody>
+              </Col>
+            </Row>
           </Row>
         </Card>
       </Col>
--- src/components/properties/Update.jsx
+++ src/components/properties/Update.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import "./Update.css";
 
 import axios from "axios";
@@ -104,8 +104,11 @@ function NewListingForm({
 
   async function postListing(data) {
     try {
-      await axios.patch(`https://makazi-api.herokuapp.com/api/v1/listings/${id}`, data);
-      window.location = '/listings';
+      await axios.patch(
+        `https://makazi-api.herokuapp.com/api/v1/listings/${id}`,
+        data
+      );
+      window.location = "/listings";
     } catch (error) {
       console.error(error);
     }
@@ -128,207 +131,216 @@ function NewListingForm({
 
   return (
     <Card style={{ height: "100%" }} className="py-2 px-5 m-5 contiainer-fluid">
-      <Row >
-        <Col md={4}>
-          <Card style={{ height: "auto" }}>
-            <img
-              src={urlData ? urlData : imgUrl}
-              style={{ width: "100%" }}
-              alt=""
-            />
-            <CardText className="p-1">
-              When you upload a new image, changes will appear here
-            </CardText>
-          </Card>
+      <Row>
+        <Col md={12}>
+          <Link to="/listings">
+            <Button>
+              <i class="fa-solid fa-chevron-left"></i> Back
+            </Button>
+          </Link>
         </Col>
-        <Col md={8}>
-          <Form onSubmit={handleSubmit}>
-            <Row>
-              <Col md={6}>
-                <FormGroup>
-                  <Label for="name">Name</Label>
-
-                  <Input
-                    onChange={handleChange}
-                    value={listingData.name}
-                    id="name"
-                    name="name"
-                    placeholder="Listing Name"
-                    type="text"
-                  />
-                </FormGroup>
-              </Col>
-              <Col md={6}>
-                <FormGroup>
-                  <Label for="size">Size</Label>
-
-                  <Input
-                    onChange={handleChange}
-                    value={listingData.size}
-                    id="size"
-                    name="size"
-                    placeholder="Listing Size in Square Feet"
-                    type="number"
-                    min={0}
-                  />
-                </FormGroup>
-              </Col>
-            </Row>
-            <FormGroup>
-              <Label for="summary">Summary</Label>
-
-              <Input
-                onChange={handleChange}
-                value={listingData.summary}
-                id="text"
-                name="summary"
-                type="text"
-                placeholder="3 BR 1 BA House w/ Garage"
+        <Row>
+          <Col md={4}>
+            <Card style={{ height: "auto" }}>
+              <img
+                src={urlData ? urlData : imgUrl}
+                style={{ width: "100%" }}
+                alt=""
               />
-            </FormGroup>
-            <FormGroup>
-              <Label for="price">Price</Label>
+              <CardText className="p-1">
+                When you upload a new image, changes will appear here
+              </CardText>
+            </Card>
+          </Col>
+          <Col md={8}>
+            <Form onSubmit={handleSubmit}>
+              <Row>
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="name">Name</Label>
 
-              <Input
-                onChange={handleChange}
-                value={listingData.price}
-                id="price"
-                name="price"
-                placeholder="Listing Price"
-              />
-            </FormGroup>
-            <Row>
-              <Col md={6}>
-                <FormGroup>
-                  <Label for="city">City</Label>
+                    <Input
+                      onChange={handleChange}
+                      value={listingData.name}
+                      id="name"
+                      name="name"
+                      placeholder="Listing Name"
+                      type="text"
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="size">Size</Label>
 
+                    <Input
+                      onChange={handleChange}
+                      value={listingData.size}
+                      id="size"
+                      name="size"
+                      placeholder="Listing Size in Square Feet"
+                      type="number"
+                      min={0}
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <FormGroup>
+                <Label for="summary">Summary</Label>
+
+                <Input
+                  onChange={handleChange}
+                  value={listingData.summary}
+                  id="text"
+                  name="summary"
+                  type="text"
+                  placeholder="3 BR 1 BA House w/ Garage"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="price">Price</Label>
+
+                <Input
+                  onChange={handleChange}
+                  value={listingData.price}
+                  id="price"
+                  name="price"
+                  placeholder="Listing Price"
+                />
+              </FormGroup>
+              <Row>
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="city">City</Label>
+
+                    <Input
+                      onChange={handleChange}
+                      value={listingData.city}
+                      id="city"
+                      name="city"
+                      type="text"
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md={6}>
+                  <FormGroup>
+                    <Label for="county">County</Label>
+
+                    <Input
+                      onChange={handleChange}
+                      value={listingData.county}
+                      id="county"
+                      name="county"
+                      type="text"
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={4}>
+                  <FormGroup>
+                    <Label for="bedrooms">Bedrooms</Label>
+
+                    <Input
+                      onChange={handleChange}
+                      value={listingData.bedrooms}
+                      id="bedrooms"
+                      name="bedrooms"
+                      type="number"
+                      min={0}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md={4}>
+                  <FormGroup>
+                    <Label for="bathrooms">Bathrooms</Label>
+
+                    <Input
+                      onChange={handleChange}
+                      value={listingData.bathrooms}
+                      id="bathrooms"
+                      name="bathrooms"
+                      type="number"
+                      min={0}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md={4}>
+                  <FormGroup>
+                    <Label for="category">for:</Label>
+
+                    <Input
+                      onChange={handleChange}
+                      selected={listingData.category}
+                      id="category"
+                      name="category"
+                      type="select"
+                    >
+                      <option defaultValue="Sale">Sale</option>
+                      <option>Rent</option>
+                      <option>Lease</option>
+                    </Input>
+                  </FormGroup>
+                </Col>
+              </Row>
+              <FormGroup row>
+                <Label for="type" sm={2}>
+                  Type
+                </Label>
+                <Col sm={10}>
                   <Input
                     onChange={handleChange}
-                    value={listingData.city}
-                    id="city"
-                    name="city"
-                    type="text"
-                  />
-                </FormGroup>
-              </Col>
-              <Col md={6}>
-                <FormGroup>
-                  <Label for="county">County</Label>
-
-                  <Input
-                    onChange={handleChange}
-                    value={listingData.county}
-                    id="county"
-                    name="county"
-                    type="text"
-                  />
-                </FormGroup>
-              </Col>
-            </Row>
-            <Row>
-              <Col md={4}>
-                <FormGroup>
-                  <Label for="bedrooms">Bedrooms</Label>
-
-                  <Input
-                    onChange={handleChange}
-                    value={listingData.bedrooms}
-                    id="bedrooms"
-                    name="bedrooms"
-                    type="number"
-                    min={0}
-                  />
-                </FormGroup>
-              </Col>
-              <Col md={4}>
-                <FormGroup>
-                  <Label for="bathrooms">Bathrooms</Label>
-
-                  <Input
-                    onChange={handleChange}
-                    value={listingData.bathrooms}
-                    id="bathrooms"
-                    name="bathrooms"
-                    type="number"
-                    min={0}
-                  />
-                </FormGroup>
-              </Col>
-              <Col md={4}>
-                <FormGroup>
-                  <Label for="category">for:</Label>
-
-                  <Input
-                    onChange={handleChange}
-                    selected={listingData.category}
-                    id="category"
-                    name="category"
+                    selected={listingData.type}
+                    id="type"
+                    name="type"
                     type="select"
                   >
-                    <option defaultValue="Sale">Sale</option>
-                    <option>Rent</option>
-                    <option>Lease</option>
+                    <option defaultValue>Single Family Home</option>
+                    <option>Attached House, Town House</option>
+                    <option>Apartment, Condo</option>
+                    <option>Commercial Property</option>
+                    <option>Estate</option>
+                    <option>Land Plot</option>
                   </Input>
-                </FormGroup>
-              </Col>
-            </Row>
-            <FormGroup row>
-              <Label for="type" sm={2}>
-                Type
-              </Label>
-              <Col sm={10}>
-                <Input
-                  onChange={handleChange}
-                  selected={listingData.type}
-                  id="type"
-                  name="type"
-                  type="select"
-                >
-                  <option defaultValue>Single Family Home</option>
-                  <option>Attached House, Town House</option>
-                  <option>Apartment, Condo</option>
-                  <option>Commercial Property</option>
-                  <option>Estate</option>
-                  <option>Land Plot</option>
-                </Input>
-              </Col>
-            </FormGroup>
+                </Col>
+              </FormGroup>
 
-            <FormGroup row>
-              <Label for="description" sm={2}>
-                Description
-              </Label>
-              <Col sm={10}>
-                <Input
-                  onChange={handleChange}
-                  value={listingData.description}
-                  id="description"
-                  name="description"
-                  type="textarea"
-                  rows={5}
-                />
-              </Col>
-            </FormGroup>
+              <FormGroup row>
+                <Label for="description" sm={2}>
+                  Description
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    onChange={handleChange}
+                    value={listingData.description}
+                    id="description"
+                    name="description"
+                    type="textarea"
+                    rows={5}
+                  />
+                </Col>
+              </FormGroup>
 
-            <FormGroup row>
-              <Label for="imgUrl" sm={2}>
-                File
-              </Label>
-              <Col sm={10}>
-                <Input
-                  accept="images/*"
-                  onChange={handleChange}
-                  // value={listingData.imgUrl}
-                  id="imgUrl"
-                  name="imgUrl"
-                  type="file"
-                />
-                <FormText>Add multiple images for your listing.</FormText>
-              </Col>
-            </FormGroup>
-            <Button>Save Changes</Button>
-          </Form>
-        </Col>
+              <FormGroup row>
+                <Label for="imgUrl" sm={2}>
+                  File
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    accept="images/*"
+                    onChange={handleChange}
+                    // value={listingData.imgUrl}
+                    id="imgUrl"
+                    name="imgUrl"
+                    type="file"
+                  />
+                  <FormText>Add multiple images for your listing.</FormText>
+                </Col>
+              </FormGroup>
+              <Button>Save Changes</Button>
+            </Form>
+          </Col>
+        </Row>
       </Row>
     </Card>
   );
